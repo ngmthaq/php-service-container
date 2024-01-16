55bdcf5c9eaa841c1773ee75097d62482b8f6145
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-01-16 21:19:54 +0700

    Update application logic

--- src/Application.php
+++ src/Application.php
@@ -11,29 +11,4 @@ class Application
     public function run(): void
     {
     }
-
-    public function resolve(string $name): mixed
-    {
-        if (empty($providers[$name])) throw new Exception("Cannot resolve '$name' in provider");
-        $item = $this->providers[$name];
-        $instance = isset($item["instance"]) ? $item["instance"] : $this->initInstance($name);
-
-        return $instance;
-    }
-
-    public function initInstance(string $name): mixed
-    {
-        $item = $this->providers[$name];
-        $callback = $item["callback"];
-        $instance = $callback();
-        $this->providers[$name]["instance"] = $instance;
-
-        return $instance;
-    }
-
-    public function register(string $name, callable $callback): void
-    {
-        $instance = null;
-        $this->providers[$name] = compact("callback", "instance");
-    }
 }
