4b882db80ea5f25259061250116aaf8c8f365d05
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2024-01-18 17:15:36 +0700

    Update IoC

--- public/index.php
+++ public/index.php
@@ -7,14 +7,13 @@ require_once "../vendor/autoload.php";
 
 try {
     ob_start();
-
     define("__ROOT__", realpath("../"));
-
     app()->registerProvider(AppServiceProvider::class);
-
     app()->registerProvider(RouterServiceProvider::class);
-
     app()->run();
 } catch (\Throwable $th) {
-    return json(["message" => $th->getMessage()], $th->getCode());
+    $message = ["message" => $th->getMessage(), "trace" => $th->getTrace()];
+    if ($_ENV["APP_ENVIRONMENT"] === "production") unset($message["trace"]);
+    json($message, $th->getCode());
+    die();
 }
